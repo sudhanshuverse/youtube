0b818b8d1b405a35903ea8a5f538151f4b4f433f
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-20 13:05:46 +0530

    App.jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from "react"
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
-import Home from './pages/Home';
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Videos from "./pages/Videos";
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [query, setQuery] = useState("Philosophy");
 
   return (
     <>
       <Header isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} setQuery={setQuery} />
       <Sidebar isSidebarOpen={isSidebarOpen} query={query} setQuery={setQuery} />
-      <Home isSidebarOpen={isSidebarOpen} query={query} />
+      <Routes>
+        <Route path="/" element={<Home isSidebarOpen={isSidebarOpen} query={query} />} />
+        <Route path="/video/:id" element={<Videos isSidebarOpen={isSidebarOpen} />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
