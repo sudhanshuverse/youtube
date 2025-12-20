a2e41075b599a60228d229daf13c4783cad39053
Sudhanshu Kumar <sudhanshu95084145@gmail.com>
2025-12-20 13:04:49 +0530

    api.js file updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,15 +12,16 @@ export async function fetchVideos(query) {
 /* Video stats (views + duration) */
 export async function fetchVideoDetails(videoIds) {
   const res = await fetch(
-    `${BASE_URL}/videos?part=statistics,contentDetails&id=${videoIds.join(",")}&key=${API_KEY}`
+    `${BASE_URL}/videos?part=snippet,statistics,contentDetails&id=${videoIds.join(",")}&key=${API_KEY}`
   );
   return res.json();
 }
 
+
 /* Channel data (image only) */
 export async function fetchChannelDetails(channelId) {
   const res = await fetch(
-    `${BASE_URL}/channels?part=snippet&id=${channelId}&key=${API_KEY}`
+    `${BASE_URL}/channels?part=snippet,statistics&id=${channelId}&key=${API_KEY}`
   );
   const data = await res.json();
   return data.items[0];
